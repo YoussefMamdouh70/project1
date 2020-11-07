6b7c16dd2ef283934400e85acbfdfbf0a339b37e
YoussefMamdouh70 <youssef1701124@miuegypt.edu.eg>
2020-11-07 11:00:59 +0200

    ghghg

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ float x;
+float y;
+char z;
+
+   char a='*';
+   char b ='/' ;
+   char c ='+';
+   char d ='-';
+
+   printf("choose letter\n");
+
+   scanf(" %c",&z);
+
+   if(z=='a')
+   {
+       scanf(" %d",&x);
+       scanf(" %d",&y);
+       printf("x*y=%d",x*y);
+
+   }
+   else if (z=='b')
+   {
+       scanf(" %d",x);
+       scanf(" %d",y);
+       printf("x/y=%d",x/y);
+   }
+   else if (z=='c')
+   {
+       scanf(" %d",x);
+       scanf(" %d",y);
+       printf("x+y=%d",x+y);
+
+   }
+   else if (z=='d')
+   {
+       scanf(" %d",x);
+       scanf("
+             %d",y);
+       printf("x-y=%d",x-y);
+   }
+
+
+}
